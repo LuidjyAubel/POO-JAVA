d3dab0aa3e199968cc42a51bee6b8efecb7b87f7
unknown <luidjy.aubel@gmail.com>
2021-11-06 00:08:17 +0100

    2

--- APP/src/Main.java
+++ APP/src/Main.java
@@ -6,6 +6,7 @@ public class Main {
 		Personne P3 = new Personne("Grimes", "Carl", 16);
 		Personne P1 = new Personne("Grimes", "Rick", 35);
 		Zombie Z1 = new Zombie(1, "normal");
+		Zombie Z2 = new Zombie(2, "radiation");
 		System.out.println("Vous êtes dans une "+PI1.getType());
 		System.out.println("le Zombie n° "+Z1.getId()+" est de type : "+Z1.getType()+" avec "+Z1.getVie()+" point de vie");
 		P1.attaqueZ(Z1);
